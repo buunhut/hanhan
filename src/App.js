950cff322e715f885d49e2374816b140b531b6b1
zero <zero@zeros-MacBook-Pro.local>
2023-10-03 14:38:35 +0700

    hanhan build v1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,389 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className="myContainer">
+        <div className="topMenu">
+
+          <div className="viTri">
+            <i class="fa-solid fa-location-dot myLogoViTri"></i>
+            <p className="cuaHang">Bách hoá HÂN HÂN</p>
+            <i class="fa-solid fa-caret-down myDropdown"></i>
+          </div>
+
+
+
+          <div className="search">
+            <input type="text" placeholder="Xin chào, bạn muốn tìm gì hôm nay?" />
+            <i class="fa-solid fa-magnifying-glass myDropdow"></i>
+
+          </div>
+
+          <div className="gioHang">
+            <i class="fa-solid fa-cart-shopping"></i>
+          </div>
+
+        </div>
+      </div>
+
+
+      <div className="myContainer">
+        <div className="mainContent">
+          <div className="proItem">
+            <div className="hinhAnh">
+              <img src="https://acecookvietnam.vn/wp-content/uploads/2017/07/H%E1%BA%A3o-H%E1%BA%A3o-T%C3%B4m-chua-cay_down33_.png" alt="" />
+            </div>
+            <div className="text">
+              <div className="tenSp">
+                <p>Gói mì hảo hảo</p>
+                <p>Gói mì hảo hảo</p>
+              </div>
+              <div className="giaSp">
+                <p className="giaBan">250,000</p>
+                <p className="giaVon">280,000</p>
+              </div>
+              <div className="myButton">
+                <button>Thêm vào giỏ</button>
+              </div>
+              <div className="giamGia">
+                <p>-20%</p>
+              </div>
+              <div className="heart">
+                <i class="fa-regular fa-heart"></i>
+              </div>
+              <div className="gif">
+                <i class="fa-solid fa-gift"></i>
+              </div>
+            </div>
+          </div>
+          <div className="proItem">
+            <div className="hinhAnh">
+              <img src="https://acecookvietnam.vn/wp-content/uploads/2017/07/H%E1%BA%A3o-H%E1%BA%A3o-T%C3%B4m-chua-cay_down33_.png" alt="" />
+            </div>
+            <div className="text">
+              <div className="tenSp">
+                <p>Gói mì hảo hảo</p>
+                <p>Gói mì hảo hảo</p>
+              </div>
+              <div className="giaSp">
+                <p className="giaBan">250,000</p>
+                <p className="giaVon">280,000</p>
+              </div>
+              <div className="myButton">
+                <button>Thêm vào giỏ</button>
+              </div>
+              <div className="giamGia">
+                <p>-20%</p>
+              </div>
+              <div className="heart">
+                <i class="fa-regular fa-heart"></i>
+              </div>
+              <div className="gif">
+                <i class="fa-solid fa-gift"></i>
+              </div>
+            </div>
+          </div>
+          <div className="proItem">
+            <div className="hinhAnh">
+              <img src="https://acecookvietnam.vn/wp-content/uploads/2017/07/H%E1%BA%A3o-H%E1%BA%A3o-T%C3%B4m-chua-cay_down33_.png" alt="" />
+            </div>
+            <div className="text">
+              <div className="tenSp">
+                <p>Gói mì hảo hảo</p>
+                <p>Gói mì hảo hảo</p>
+              </div>
+              <div className="giaSp">
+                <p className="giaBan">250,000</p>
+                <p className="giaVon">280,000</p>
+              </div>
+              <div className="myButton">
+                <button>Thêm vào giỏ</button>
+              </div>
+              <div className="giamGia">
+                <p>-20%</p>
+              </div>
+              <div className="heart">
+                <i class="fa-regular fa-heart"></i>
+              </div>
+              <div className="gif">
+                <i class="fa-solid fa-gift"></i>
+              </div>
+            </div>
+          </div>
+          <div className="proItem">
+            <div className="hinhAnh">
+              <img src="https://acecookvietnam.vn/wp-content/uploads/2017/07/H%E1%BA%A3o-H%E1%BA%A3o-T%C3%B4m-chua-cay_down33_.png" alt="" />
+            </div>
+            <div className="text">
+              <div className="tenSp">
+                <p>Gói mì hảo hảo</p>
+                <p>Gói mì hảo hảo</p>
+              </div>
+              <div className="giaSp">
+                <p className="giaBan">250,000</p>
+                <p className="giaVon">280,000</p>
+              </div>
+              <div className="myButton">
+                <button>Thêm vào giỏ</button>
+              </div>
+              <div className="giamGia">
+                <p>-20%</p>
+              </div>
+              <div className="heart">
+                <i class="fa-regular fa-heart"></i>
+              </div>
+              <div className="gif">
+                <i class="fa-solid fa-gift"></i>
+              </div>
+            </div>
+          </div>
+          <div className="proItem">
+            <div className="hinhAnh">
+              <img src="https://acecookvietnam.vn/wp-content/uploads/2017/07/H%E1%BA%A3o-H%E1%BA%A3o-T%C3%B4m-chua-cay_down33_.png" alt="" />
+            </div>
+            <div className="text">
+              <div className="tenSp">
+                <p>Gói mì hảo hảo</p>
+                <p>Gói mì hảo hảo</p>
+              </div>
+              <div className="giaSp">
+                <p className="giaBan">250,000</p>
+                <p className="giaVon">280,000</p>
+              </div>
+              <div className="myButton">
+                <button>Thêm vào giỏ</button>
+              </div>
+              <div className="giamGia">
+                <p>-20%</p>
+              </div>
+              <div className="heart">
+                <i class="fa-regular fa-heart"></i>
+              </div>
+              <div className="gif">
+                <i class="fa-solid fa-gift"></i>
+              </div>
+            </div>
+          </div>
+          <div className="proItem">
+            <div className="hinhAnh">
+              <img src="https://acecookvietnam.vn/wp-content/uploads/2017/07/H%E1%BA%A3o-H%E1%BA%A3o-T%C3%B4m-chua-cay_down33_.png" alt="" />
+            </div>
+            <div className="text">
+              <div className="tenSp">
+                <p>Gói mì hảo hảo</p>
+                <p>Gói mì hảo hảo</p>
+              </div>
+              <div className="giaSp">
+                <p className="giaBan">250,000</p>
+                <p className="giaVon">280,000</p>
+              </div>
+              <div className="myButton">
+                <button>Thêm vào giỏ</button>
+              </div>
+              <div className="giamGia">
+                <p>-20%</p>
+              </div>
+              <div className="heart">
+                <i class="fa-regular fa-heart"></i>
+              </div>
+              <div className="gif">
+                <i class="fa-solid fa-gift"></i>
+              </div>
+            </div>
+          </div>
+          <div className="proItem">
+            <div className="hinhAnh">
+              <img src="https://acecookvietnam.vn/wp-content/uploads/2017/07/H%E1%BA%A3o-H%E1%BA%A3o-T%C3%B4m-chua-cay_down33_.png" alt="" />
+            </div>
+            <div className="text">
+              <div className="tenSp">
+                <p>Gói mì hảo hảo</p>
+                <p>Gói mì hảo hảo</p>
+              </div>
+              <div className="giaSp">
+                <p className="giaBan">250,000</p>
+                <p className="giaVon">280,000</p>
+              </div>
+              <div className="myButton">
+                <button>Thêm vào giỏ</button>
+              </div>
+              <div className="giamGia">
+                <p>-20%</p>
+              </div>
+              <div className="heart">
+                <i class="fa-regular fa-heart"></i>
+              </div>
+              <div className="gif">
+                <i class="fa-solid fa-gift"></i>
+              </div>
+            </div>
+          </div>
+          <div className="proItem">
+            <div className="hinhAnh">
+              <img src="https://acecookvietnam.vn/wp-content/uploads/2017/07/H%E1%BA%A3o-H%E1%BA%A3o-T%C3%B4m-chua-cay_down33_.png" alt="" />
+            </div>
+            <div className="text">
+              <div className="tenSp">
+                <p>Gói mì hảo hảo</p>
+                <p>Gói mì hảo hảo</p>
+              </div>
+              <div className="giaSp">
+                <p className="giaBan">250,000</p>
+                <p className="giaVon">280,000</p>
+              </div>
+              <div className="myButton">
+                <button>Thêm vào giỏ</button>
+              </div>
+              <div className="giamGia">
+                <p>-20%</p>
+              </div>
+              <div className="heart">
+                <i class="fa-regular fa-heart"></i>
+              </div>
+              <div className="gif">
+                <i class="fa-solid fa-gift"></i>
+              </div>
+            </div>
+          </div>
+          <div className="proItem">
+            <div className="hinhAnh">
+              <img src="https://acecookvietnam.vn/wp-content/uploads/2017/07/H%E1%BA%A3o-H%E1%BA%A3o-T%C3%B4m-chua-cay_down33_.png" alt="" />
+            </div>
+            <div className="text">
+              <div className="tenSp">
+                <p>Gói mì hảo hảo</p>
+                <p>Gói mì hảo hảo</p>
+              </div>
+              <div className="giaSp">
+                <p className="giaBan">250,000</p>
+                <p className="giaVon">280,000</p>
+              </div>
+              <div className="myButton">
+                <button>Thêm vào giỏ</button>
+              </div>
+              <div className="giamGia">
+                <p>-20%</p>
+              </div>
+              <div className="heart">
+                <i class="fa-regular fa-heart"></i>
+              </div>
+              <div className="gif">
+                <i class="fa-solid fa-gift"></i>
+              </div>
+            </div>
+          </div>
+          <div className="proItem">
+            <div className="hinhAnh">
+              <img src="https://acecookvietnam.vn/wp-content/uploads/2017/07/H%E1%BA%A3o-H%E1%BA%A3o-T%C3%B4m-chua-cay_down33_.png" alt="" />
+            </div>
+            <div className="text">
+              <div className="tenSp">
+                <p>Gói mì hảo hảo</p>
+                <p>Gói mì hảo hảo</p>
+              </div>
+              <div className="giaSp">
+                <p className="giaBan">250,000</p>
+                <p className="giaVon">280,000</p>
+              </div>
+              <div className="myButton">
+                <button>Thêm vào giỏ</button>
+              </div>
+              <div className="giamGia">
+                <p>-20%</p>
+              </div>
+              <div className="heart">
+                <i class="fa-regular fa-heart"></i>
+              </div>
+              <div className="gif">
+                <i class="fa-solid fa-gift"></i>
+              </div>
+            </div>
+          </div>
+          <div className="proItem">
+            <div className="hinhAnh">
+              <img src="https://acecookvietnam.vn/wp-content/uploads/2017/07/H%E1%BA%A3o-H%E1%BA%A3o-T%C3%B4m-chua-cay_down33_.png" alt="" />
+            </div>
+            <div className="text">
+              <div className="tenSp">
+                <p>Gói mì hảo hảo</p>
+                <p>Gói mì hảo hảo</p>
+              </div>
+              <div className="giaSp">
+                <p className="giaBan">250,000</p>
+                <p className="giaVon">280,000</p>
+              </div>
+              <div className="myButton">
+                <button>Thêm vào giỏ</button>
+              </div>
+              <div className="giamGia">
+                <p>-20%</p>
+              </div>
+              <div className="heart">
+                <i class="fa-regular fa-heart"></i>
+              </div>
+              <div className="gif">
+                <i class="fa-solid fa-gift"></i>
+              </div>
+            </div>
+          </div>
+          <div className="proItem">
+            <div className="hinhAnh">
+              <img src="https://acecookvietnam.vn/wp-content/uploads/2017/07/H%E1%BA%A3o-H%E1%BA%A3o-T%C3%B4m-chua-cay_down33_.png" alt="" />
+            </div>
+            <div className="text">
+              <div className="tenSp">
+                <p>Gói mì hảo hảo</p>
+                <p>Gói mì hảo hảo</p>
+              </div>
+              <div className="giaSp">
+                <p className="giaBan">250,000</p>
+                <p className="giaVon">280,000</p>
+              </div>
+              <div className="myButton">
+                <button>Thêm vào giỏ</button>
+              </div>
+              <div className="giamGia">
+                <p>-20%</p>
+              </div>
+              <div className="heart">
+                <i class="fa-regular fa-heart"></i>
+              </div>
+              <div className="gif">
+                <i class="fa-solid fa-gift"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="myContainer">
+        <div className="buttonMenu">
+          <div className="trangChu">
+            trang chủ
+          </div>
+          <div className="danhMuc">
+            danh mục
+          </div>
+          <div className="thuongHieu">
+            thương hiệu
+          </div>
+          <div className="gioHang">
+            gioHang
+          </div>
+          <div className="taiKhoan">
+            taiKhoan
+          </div>
+        </div>
+      </div>
+
+
+
+
+
+
+
     </div>
   );
 }
