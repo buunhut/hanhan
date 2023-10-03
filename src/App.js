79afd8075c8b72a1241f737506127a0cde03b64b
zero <zero@zeros-MacBook-Pro.local>
2023-10-03 16:23:11 +0700

    hanhan build v9

--- src/App.js
+++ src/App.js
@@ -1,39 +1,38 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-
+      <h1>Đang viết trên thiết bị mobile :)</h1>
       <div className="myContainer">
         <div className="topMenu">
-
           <div className="viTri">
             <i class="fa-solid fa-location-dot myLogoViTri"></i>
             <p className="cuaHang">Bách hoá HÂN HÂN</p>
             <i class="fa-solid fa-caret-down myDropdown"></i>
           </div>
-
-
-
           <div className="search">
-            <input type="text" placeholder="Xin chào, bạn muốn tìm gì hôm nay?" />
+            <input
+              type="text"
+              placeholder="Xin chào, bạn muốn tìm gì hôm nay?"
+            />
             <i class="fa-solid fa-magnifying-glass myDropdow"></i>
-
           </div>
 
           <div className="gioHang">
             <i class="fa-solid fa-cart-shopping"></i>
           </div>
-
         </div>
       </div>
-
 
       <div className="myContainer">
         <div className="mainContent">
           <div className="proItem">
             <div className="hinhAnh">
-              <img src="https://acecookvietnam.vn/wp-content/uploads/2017/07/H%E1%BA%A3o-H%E1%BA%A3o-T%C3%B4m-chua-cay_down33_.png" alt="" />
+              <img
+                src="https://acecookvietnam.vn/wp-content/uploads/2017/07/H%E1%BA%A3o-H%E1%BA%A3o-T%C3%B4m-chua-cay_down33_.png"
+                alt=""
+              />
             </div>
             <div className="text">
               <div className="tenSp">
@@ -60,7 +59,10 @@ function App() {
           </div>
           <div className="proItem">
             <div className="hinhAnh">
-              <img src="https://acecookvietnam.vn/wp-content/uploads/2017/07/H%E1%BA%A3o-H%E1%BA%A3o-T%C3%B4m-chua-cay_down33_.png" alt="" />
+              <img
+                src="https://acecookvietnam.vn/wp-content/uploads/2017/07/H%E1%BA%A3o-H%E1%BA%A3o-T%C3%B4m-chua-cay_down33_.png"
+                alt=""
+              />
             </div>
             <div className="text">
               <div className="tenSp">
@@ -87,7 +89,10 @@ function App() {
           </div>
           <div className="proItem">
             <div className="hinhAnh">
-              <img src="https://acecookvietnam.vn/wp-content/uploads/2017/07/H%E1%BA%A3o-H%E1%BA%A3o-T%C3%B4m-chua-cay_down33_.png" alt="" />
+              <img
+                src="https://acecookvietnam.vn/wp-content/uploads/2017/07/H%E1%BA%A3o-H%E1%BA%A3o-T%C3%B4m-chua-cay_down33_.png"
+                alt=""
+              />
             </div>
             <div className="text">
               <div className="tenSp">
@@ -101,9 +106,7 @@ function App() {
               <div className="myButton">
                 <button>Thêm vào giỏ</button>
               </div>
-              <div className="giamGia">
-                {/* <p>-20%</p> */}
-              </div>
+              <div className="giamGia">{/* <p>-20%</p> */}</div>
               <div className="heart">
                 <i class="fa-regular fa-heart"></i>
               </div>
@@ -114,7 +117,10 @@ function App() {
           </div>
           <div className="proItem">
             <div className="hinhAnh">
-              <img src="https://acecookvietnam.vn/wp-content/uploads/2017/07/H%E1%BA%A3o-H%E1%BA%A3o-T%C3%B4m-chua-cay_down33_.png" alt="" />
+              <img
+                src="https://acecookvietnam.vn/wp-content/uploads/2017/07/H%E1%BA%A3o-H%E1%BA%A3o-T%C3%B4m-chua-cay_down33_.png"
+                alt=""
+              />
             </div>
             <div className="text">
               <div className="tenSp">
@@ -141,7 +147,10 @@ function App() {
           </div>
           <div className="proItem">
             <div className="hinhAnh">
-              <img src="https://acecookvietnam.vn/wp-content/uploads/2017/07/H%E1%BA%A3o-H%E1%BA%A3o-T%C3%B4m-chua-cay_down33_.png" alt="" />
+              <img
+                src="https://acecookvietnam.vn/wp-content/uploads/2017/07/H%E1%BA%A3o-H%E1%BA%A3o-T%C3%B4m-chua-cay_down33_.png"
+                alt=""
+              />
             </div>
             <div className="text">
               <div className="tenSp">
@@ -168,7 +177,10 @@ function App() {
           </div>
           <div className="proItem">
             <div className="hinhAnh">
-              <img src="https://acecookvietnam.vn/wp-content/uploads/2017/07/H%E1%BA%A3o-H%E1%BA%A3o-T%C3%B4m-chua-cay_down33_.png" alt="" />
+              <img
+                src="https://acecookvietnam.vn/wp-content/uploads/2017/07/H%E1%BA%A3o-H%E1%BA%A3o-T%C3%B4m-chua-cay_down33_.png"
+                alt=""
+              />
             </div>
             <div className="text">
               <div className="tenSp">
@@ -182,19 +194,19 @@ function App() {
               <div className="myButton">
                 <button>Thêm vào giỏ</button>
               </div>
-              <div className="giamGia">
-                {/* <p>-20%</p> */}
-              </div>
+              <div className="giamGia">{/* <p>-20%</p> */}</div>
               <div className="heart">
                 <i class="fa-regular fa-heart"></i>
               </div>
-              <div className="gif">
-              </div>
+              <div className="gif"></div>
             </div>
           </div>
           <div className="proItem">
             <div className="hinhAnh">
-              <img src="https://acecookvietnam.vn/wp-content/uploads/2017/07/H%E1%BA%A3o-H%E1%BA%A3o-T%C3%B4m-chua-cay_down33_.png" alt="" />
+              <img
+                src="https://acecookvietnam.vn/wp-content/uploads/2017/07/H%E1%BA%A3o-H%E1%BA%A3o-T%C3%B4m-chua-cay_down33_.png"
+                alt=""
+              />
             </div>
             <div className="text">
               <div className="tenSp">
@@ -221,7 +233,10 @@ function App() {
           </div>
           <div className="proItem">
             <div className="hinhAnh">
-              <img src="https://acecookvietnam.vn/wp-content/uploads/2017/07/H%E1%BA%A3o-H%E1%BA%A3o-T%C3%B4m-chua-cay_down33_.png" alt="" />
+              <img
+                src="https://acecookvietnam.vn/wp-content/uploads/2017/07/H%E1%BA%A3o-H%E1%BA%A3o-T%C3%B4m-chua-cay_down33_.png"
+                alt=""
+              />
             </div>
             <div className="text">
               <div className="tenSp">
@@ -248,7 +263,10 @@ function App() {
           </div>
           <div className="proItem">
             <div className="hinhAnh">
-              <img src="https://acecookvietnam.vn/wp-content/uploads/2017/07/H%E1%BA%A3o-H%E1%BA%A3o-T%C3%B4m-chua-cay_down33_.png" alt="" />
+              <img
+                src="https://acecookvietnam.vn/wp-content/uploads/2017/07/H%E1%BA%A3o-H%E1%BA%A3o-T%C3%B4m-chua-cay_down33_.png"
+                alt=""
+              />
             </div>
             <div className="text">
               <div className="tenSp">
@@ -275,7 +293,10 @@ function App() {
           </div>
           <div className="proItem">
             <div className="hinhAnh">
-              <img src="https://acecookvietnam.vn/wp-content/uploads/2017/07/H%E1%BA%A3o-H%E1%BA%A3o-T%C3%B4m-chua-cay_down33_.png" alt="" />
+              <img
+                src="https://acecookvietnam.vn/wp-content/uploads/2017/07/H%E1%BA%A3o-H%E1%BA%A3o-T%C3%B4m-chua-cay_down33_.png"
+                alt=""
+              />
             </div>
             <div className="text">
               <div className="tenSp">
@@ -302,7 +323,10 @@ function App() {
           </div>
           <div className="proItem">
             <div className="hinhAnh">
-              <img src="https://acecookvietnam.vn/wp-content/uploads/2017/07/H%E1%BA%A3o-H%E1%BA%A3o-T%C3%B4m-chua-cay_down33_.png" alt="" />
+              <img
+                src="https://acecookvietnam.vn/wp-content/uploads/2017/07/H%E1%BA%A3o-H%E1%BA%A3o-T%C3%B4m-chua-cay_down33_.png"
+                alt=""
+              />
             </div>
             <div className="text">
               <div className="tenSp">
@@ -329,7 +353,10 @@ function App() {
           </div>
           <div className="proItem">
             <div className="hinhAnh">
-              <img src="https://acecookvietnam.vn/wp-content/uploads/2017/07/H%E1%BA%A3o-H%E1%BA%A3o-T%C3%B4m-chua-cay_down33_.png" alt="" />
+              <img
+                src="https://acecookvietnam.vn/wp-content/uploads/2017/07/H%E1%BA%A3o-H%E1%BA%A3o-T%C3%B4m-chua-cay_down33_.png"
+                alt=""
+              />
             </div>
             <div className="text">
               <div className="tenSp">
@@ -364,45 +391,33 @@ function App() {
               <i class="fa-solid fa-house"></i>
             </div>
             <p>Trang chủ</p>
-
           </div>
           <div className="buttonItem">
             <div>
               <i class="fa-solid fa-layer-group"></i>
             </div>
             <p>Danh mục</p>
-
           </div>
           <div className="buttonItem">
             <div>
               <i class="fa-solid fa-bag-shopping"></i>
             </div>
             <p>Thương hiệu</p>
-
           </div>
           <div className="buttonItem">
             <div>
               <i class="fa-solid fa-bell"></i>
             </div>
             <p>Thông báo</p>
-
           </div>
           <div className="buttonItem">
             <div>
               <i class="fa-solid fa-user"></i>
             </div>
             <p>Tài khoản</p>
-
           </div>
         </div>
       </div>
-
-
-
-
-
-
-
     </div>
   );
 }
